edb833e947a0744b478f7f2bc762f0c3811d9846
ArjunParikh1404 <arjunparikh1404@gmail.com>
2026-04-25 12:08:30 -0400

    Add gradient function for partial derivatives

--- Calculus_ML.py
+++ Calculus_ML.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-print("=== CALCULUS FOR MACHINE LEARNING ===")
-
-
 # Numerical Derivative
 
 def derivative(f, x, h=1e-5):
@@ -14,3 +11,16 @@ x=2
 
 print("\nDerivative of f(x)=x²+3x at x=2")
 print(derivative(f,x))
+
+
+# Partial Derivatives / Gradient
+# f(x,y)=x² + y²
+# grad=[2x,2y]
+
+def gradient(x,y):
+    dfdx=2*x
+    dfdy=2*y
+    return np.array([dfdx,dfdy])
+
+print("\nGradient at (3,4):")
+print(gradient(3,4))
