92f44cc291c33fd81fa47a813db1bd7d138a9eee
ArjunParikh1404 <arjunparikh1404@gmail.com>
2026-05-29 23:01:56 -0400

    Delete Remove_Duplicates.py

--- Remove_Duplicates.py
+++
@@ -1,7 +0,0 @@
-# This program takes user input, removes duplicate values using set(), and prints the unique elements.
-
-arr = list(map(int, input("Enter numbers separated by spaces: ").split()))
-
-unique = list(set(arr))
-
-print("Unique elements:", unique)
