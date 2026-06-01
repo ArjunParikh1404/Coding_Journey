338b072c097295a4cfabad4de0b2f400f11ca251
ArjunParikh1404 <arjunparikh1404@gmail.com>
2026-05-31 23:07:24 -0400

    Add Roman to Integer conversion functionality
    Implement a function to convert Roman numerals to integers.

---
+++ Roman_To_Integer_Converter.py
@@ -0,0 +1,29 @@
+# Converts a Roman numeral entered by the user into its equivalent integer value.
+
+def roman_to_int(s):
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+
+    for i in range(len(s)):
+        if i < len(s) - 1 and roman[s[i]] < roman[s[i + 1]]:
+            total -= roman[s[i]]
+        else:
+            total += roman[s[i]]
+
+    return total
+
+
+roman_number = input("Enter a Roman numeral: ").upper()
+
+result = roman_to_int(roman_number)
+
+print("Integer value:", result)
