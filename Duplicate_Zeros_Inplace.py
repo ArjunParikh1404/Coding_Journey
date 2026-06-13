ac453de9bb38b01c047ea7038192d01999fdc774
ArjunParikh1404 <arjunparikh1404@gmail.com>
2026-06-12 22:01:28 -0400

    Add function to duplicate zeros in-place
    Implement in-place duplication of zeros in an array.

---
+++ Duplicate_Zeros_Inplace.py
@@ -0,0 +1,21 @@
+# Given a fixed-size array, duplicate each zero in-place and shift remaining elements right without exceeding array length.
+
+arr = list(map(int, input("Enter the Array : ").split()))
+
+zeros = arr.count(0)
+real = len(arr) - 1
+virtual = len(arr) - 1 + zeros
+
+while real < virtual:
+    if virtual < len(arr):
+        arr[virtual] = arr[real]
+        
+    if arr[real] == 0:
+        virtual -= 1
+        if virtual < len(arr):
+            arr[virtual] = 0
+    
+    real -= 1
+    virtual -= 1
+    
+print(arr)
