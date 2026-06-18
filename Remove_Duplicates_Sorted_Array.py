10dace9d4933798fb9b5b0d6db8eb3b356a2f60f
ArjunParikh1404 <arjunparikh1404@gmail.com>
2026-06-17 23:50:28 -0400

    Add function to remove duplicates in sorted array
    Implement function to remove duplicates from sorted array.

---
+++ Remove_Duplicates_Sorted_Array.py
@@ -0,0 +1,17 @@
+# Given a sorted array, remove duplicates in-place so each value appears once; return k = number of unique elements, and ensure first k positions contain those unique values in order.
+# Time Complexity = O(n), Space Complexity = O(1)
+
+nums = list(map(int, input("Enter the array : ").split()))
+
+p1 = 0
+p2 = 1 
+
+while p2 < len(nums):
+    if nums[p1] != nums[p2]:
+        p1 += 1
+        nums[p1] = nums[p2]
+
+    p2 += 1
+        
+print(nums)
+print("unique numbers =", p1+1)
