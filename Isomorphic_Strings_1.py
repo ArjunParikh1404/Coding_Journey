ede8c1499b3d28aafb6ff30b001a6a124b2eee79
ArjunParikh1404 <arjunparikh1404@gmail.com>
2026-08-29 23:21:11 -0400

    Add function to check if two strings are isomorphic
    Implement isomorphic string check with character mapping.

---
+++ Isomorphic_Strings_1.py
@@ -0,0 +1,24 @@
+# Determine whether two strings are isomorphic by checking if each character in one string maps uniquely and consistently to a character in the other.
+# Time complexity = O(n), Space complexity = O(n)
+# Leetcode = 205
+
+s = str(input("Enter the first string :"))
+t = str(input("Enter the second string :"))
+
+mapst = {}
+mapts = {}
+
+if len(s) != len(t):
+    print("False")
+
+else:
+    for i in range(len(s)):
+        mapst[s[i]] = t[i]
+        mapts[t[i]] = s[i]
+        
+    for j in range(len(s)):
+        if t[j] != mapst[s[j]] or s[j] != mapts[t[j]]:
+            print("False")
+            break
+    else:
+        print("True")
